3afbeea214359d41c066e535e03678712cc0ac14
trevin-livele <liveletrevin6@gmail.com>
2022-02-02 23:46:57 +0300

    added initial project

---
+++ forms.py
@@ -0,0 +1,16 @@
+#forms.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,IntegerField,SubmitField
+
+
+class AddForm(FlaskForm):
+
+    name = StringField('Name of Puppy: ')
+    submit = SubmitField('Add Puppy')
+
+
+class DelForm(FlaskForm):
+
+    id = IntegerField("Id Number of puppy to remove: ")
+    submit = SubmitField("Remove puppy")
